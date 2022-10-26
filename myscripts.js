e65b6579842d1258d4585b79505c972ff0dc2f0b
dhise <jdhise@gmail.com>
2022-10-25 22:24:33 -0400

    api test

---
+++ myscripts.js
@@ -0,0 +1,10 @@
+
+
+// The client ID is obtained from the {{ Google Cloud Console }}
+// at {{ https://cloud.google.com/console }}.
+// If you run this code from a server other than http://localhost,
+// you need to register your own client ID.
+var OAUTH2_CLIENT_ID = '625025248155-d8ec2iiuu9uuc56ot2kf9a6is8ruge25.apps.googleusercontent.com';
+var OAUTH2_SCOPES = [
+ 'https://www.googleapis.com/auth/youtube'
+];
